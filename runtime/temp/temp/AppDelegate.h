a4c2a0ae3614b604afb0260eeba53854ca11fe3b
liaoxf <liao0322@hotmail.com>
2017-10-09 20:16:24 +0800

    nothing

---
+++ runtime/temp/temp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  temp
+//
+//  Created by liaoxf on 2017/10/9.
+//  Copyright © 2017年 liaoxf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtime/temp/temp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  temp
+//
+//  Created by liaoxf on 2017/10/9.
+//  Copyright © 2017年 liaoxf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
